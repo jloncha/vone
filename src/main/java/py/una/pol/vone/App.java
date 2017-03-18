fd289917fe20310bf25c92709e99b73cca747bee
jloncha <jloncha@gmail.com>
2017-03-18 11:34:29 -0300

    first commit

---
+++ src/main/java/py/una/pol/vone/App.java
@@ -0,0 +1,13 @@
+package py.una.pol.vone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
