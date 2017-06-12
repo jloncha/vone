7864244092c0bea8dc72f5a9861cf7771a090360
jloncha <jloncha@gmail.com>
2017-06-12 16:15:05 -0400

    Commit para arreglar diferencias

--- src/main/java/py/una/pol/vone/App.java
+++ src/main/java/py/una/pol/vone/App.java
@@ -1,6 +1,6 @@
 package py.una.pol.vone;
 
-import py.una.pol.vone.aco.ACO;
+import py.una.pol.vone.sa.Core;
 import py.una.pol.vone.util.CargarRed;
 
 /**
@@ -12,8 +12,8 @@ public class App
     public static void main( String[] args )
     {
         CargarRed red = new CargarRed();
-        ACO aco = new ACO(50, 5, red.redVirtual, red.redFisica);
-        aco.solve();
+        Core problema = new Core(red.redVirtual, red.redFisica);
+		problema.generateFirstSolution();
 
     }
 }
